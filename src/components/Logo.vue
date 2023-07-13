3ebb27e50805411477893bc88a0f814c3577fb03
Ben Dyer <ben@atomscale.co>
2023-07-14 00:02:35 +0100

    small tweaks

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,8 +1,7 @@
 <template>
-    <router-link :to="{name: 'Landing'}">
-    <div class="flex flex-shrink-0 items-center">
-        <img class="h-9 pb-1 w-auto" src="@/assets/9p-logo-empty.png" alt="Hydrogen Web" />
-        <!-- <div class="text-blue-400 pl-2 pt-0 font-bold">Nine Panels</div> -->
-    </div>
-</router-link>
+    <router-link :to="{ name: 'Landing' }">
+        <div class="flex flex-shrink-0 items-center">
+            <img class="h-8 pb-1 w-auto" src="@/assets/9p-logo-empty.png" alt="9p" />
+        </div>
+    </router-link>
 </template>
