12bb19dab8bfe86292dac7a582951b2bd6da95db
Ben Dyer <ben@atomscale.co>
2023-07-13 08:14:34 +0100

    testing vercel staging

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -6,7 +6,7 @@
         <div>
             <h1 class="text-center mt-12 text-4xl mb-12 font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                 <span class="block text-gray-600">Consistency</span>
-                <span class="block text-gray-600">vigilance needs visibility</span>
+                <span class="block text-gray-600">vigilance v3</span>
             </h1>
 
                     <div class="mx-auto  max-w-7xl px-4 sm:mt-24 sm:px-6 ">
