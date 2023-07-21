65e50ecb50edfd8edddb696af9d91a9fb8659994
Ben Dyer <ben@atomscale.co>
2023-07-21 07:27:52 +0100

    ui tweaks

--- src/components/PanelGridFrame.vue
+++ src/components/PanelGridFrame.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid grid-cols-3 gap-1 px-2 py-2 md:px-0 md:w-3/5 mx-auto">
-    <div v-for="i in 9" :key="i" class="aspect-w-1 aspect-h-1 rounded-md">
+    <div v-for="i in 9" :key="i" class="aspect-w-1 aspect-h-1 rounded-lg">
       <div class="aspect-content">
 
         <Panel v-if="this.Store.panels[i - 1]" :panel="this.Store.panels[i - 1]" />
         <PanelGridAdd v-else-if="this.Store.panels.length == (i -1)"/>
-        <div v-else class="flex items-center justify-center text-gray-500 border border-dashed border-gray-300 text-sm rounded-md">
+        <div v-else class="flex items-center justify-center text-gray-500 border border-dashed border-gray-300 text-sm rounded-lg">
         </div>
       </div>
     </div>
