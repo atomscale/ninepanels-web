32732bdc02642a9f5a8296a582039f2e3cc70347
Ben Dyer <ben@atomscale.co>
2023-09-03 09:01:32 +0100

    change v-if

--- src/views/SignedOut/PasswordResetView.vue
+++ src/views/SignedOut/PasswordResetView.vue
@@ -1,6 +1,6 @@
 <template >
 
-  <PasswordResetForm v-if="this.$route.query.user_id && this.$route.query.password_reset_token" />
+  <PasswordResetForm v-if="this.$route.query.email && this.$route.query.password_reset_token" />
   <PasswordResetRequestForm v-else />
 
 </template>
