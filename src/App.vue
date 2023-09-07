00e558f6c169bef0ec5fc0f4288c57fb7c7f022b
Ben Dyer <ben@atomscale.co>
2023-09-07 14:25:49 +0100

    remove outage banner

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <div class="flex flex-col items-start fixed top-2 left-12 z-50" v-if="Store.messages">
     <FlashMessage v-for="message in Store.messages" :key="message.message" :message="message.message"
       :error="message.error" />
-      <div class="bg-white mt-1 text-sm border-2 border-red-800 shadow-lg px-2 mx-2 rounded-lg text-gray-600">9P's server hosting platform is currently experiencing an outage (it's them, not us!). Last updated: 14:09 UK</div>
+      <!-- <div class="bg-white mt-1 text-sm border-2 border-red-800 shadow-lg px-2 mx-2 rounded-lg text-gray-600">9P's server hosting platform is currently experiencing an outage (it's them, not us!). Last updated: 14:09 UK</div> -->
   </div>
 
 
