6a6c451aec063cc0db658be63776079f00d675ec
Ben Dyer <ben@atomscale.co>
2023-10-03 17:08:54 +0100

    overflow indicators

--- src/components/panels/tray/PanelDescDisplay.vue
+++ src/components/panels/tray/PanelDescDisplay.vue
@@ -1,18 +1,24 @@
 <template >
-  <div class="">
+  <div class="relative">
 
     <div class="flex justify-between ">
-      <!-- <div   class="text-sm font-light h-5 text-np-base">About</div> -->
-      <VueShowdown v-if="this.panel.description" class="text-sm mr-2 max-h-64 font-light text-np-base w-auto whitespace-pre-line overflow-hidden overflow-y-auto" flavor="vanilla" :options="{ emoji: true }"
-        :markdown="this.panel.description">
-      </VueShowdown>
-      <div v-else class="italic font-extralight text-gray-400 text-sm">Why is this important to you?</div>
+      <div ref="scrollableDiv" class="overflow-scroll" @scroll="checkScroll">
+
+        <VueShowdown v-if="this.panel.description"
+          class="text-sm mr-2 max-h-64 font-light text-np-base w-auto whitespace-pre-line " flavor="vanilla"
+          :options="{ emoji: true }" :markdown="this.panel.description">
+        </VueShowdown>
+        <div v-else class="italic font-extralight text-gray-400 text-sm">Why is this important to you?</div>
+      </div>
       <div>
         <button class="" @click="toggleEditState()">
           <PencilIcon v-if="this.panel.description" class="h-4 w-4 text-gray-400 hover:text-np-base" />
           <PlusIcon v-else class="h-5 w-5 text-gray-400 hover:text-np-base" />
         </button>
       </div>
+    </div>
+    <div v-if="showEllipsis" class="flex justify-center  transition-all duration-500">
+      <div class=" absolute -bottom-3 text-2xl text-gray-300 mr-6 ">. . .</div>
     </div>
 
 
@@ -27,6 +33,7 @@ import { VueShowdown } from 'vue-showdown'
 
 import {
   PencilIcon,
+  ArrowDownCircleIcon,
   PlusIcon
 } from '@heroicons/vue/24/outline'
 
@@ -40,14 +47,34 @@ export default {
   methods: {
     toggleEditState() {
       this.Store.panelDescEditState = !this.Store.panelDescEditState
-    }
+    },
+    checkScroll() {
+      const element = this.$refs.scrollableDiv
+      if (!element) {
+        console.error("Element not found");
+        return;
+      }
+      if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
+        this.showEllipsis = false
+      } else {
+        this.showEllipsis = true
+      }
+    },
   },
   components: {
     PencilIcon,
     PlusIcon,
+    ArrowDownCircleIcon,
     VueShowdown
   },
-
+  data() {
+    return {
+      showEllipsis: false
+    }
+  },
+  mounted() {
+    this.checkScroll()
+  },
   props: {
     description: {
       type: String,
--- src/components/panels/tray/PanelHistory.vue
+++ src/components/panels/tray/PanelHistory.vue
@@ -1,36 +1,42 @@
 <template >
-    <div class="flex flex-col w-full justify-start overflow-scroll items-center">
+    <div class="flex flex-col w-full  h-56 justify-start  items-center relative">
 
         <div class="flex justify-between items-center w-40  text-xs text-np-base font-extralight">
             <button
-                class="hover:bg-np-accent hover:text-np-inverted transition shadow-sm duration-200 border-np-base border w-full py-1 rounded-l-md"
-                :class="selected === 7 ? 'bg-np-accent border-white border text-np-inverted shadow-none scale-95' : ''"
+                class="hover:bg-np-accent hover:text-np-inverted transition shadow-sm duration-200 border border-np-base w-full py-1 rounded-l-md"
+                :class="selected === 7 ? 'bg-np-accent border-gray-300 border text-np-inverted shadow-none scale-95' : ''"
                 @click="setSelectedLimit(7)">7</button>
             <button
                 class="hover:bg-np-accent hover:text-np-inverted transition shadow-sm duration-200 border-np-base border-r border-t border-b w-full py-1"
-                :class="selected === 14 ? 'bg-np-accent border-white border text-np-inverted shadow-none scale-95' : ''"
+                :class="selected === 14 ? 'bg-np-accent border-gray-300 border text-np-inverted shadow-none scale-95' : ''"
                 @click="setSelectedLimit(14)">14</button>
             <button
                 class="hover:bg-np-accent hover:text-np-inverted transition shadow-sm duration-200 border-np-base border-t border-b w-full py-1"
-                :class="selected === 30 ? 'bg-np-accent border-white border text-np-inverted shadow-none scale-95' : ''"
+                :class="selected === 30 ? 'bg-np-accent border-gray-300 border text-np-inverted shadow-none scale-95' : ''"
                 @click="setSelectedLimit(30)">30</button>
             <button
                 class="hover:bg-np-accent hover:text-np-inverted transition shadow-sm duration-200 border-np-base border rounded-r-md w-full py-0.5 text-sm"
-                :class="selected === null ? 'bg-np-accent border-white border text-np-inverted shadow-none scale-95' : ''"
+                :class="selected === null ? 'bg-np-accent  border text-np-inverted shadow-none scale-95' : ''"
                 @click="setSelectedLimit(null)">∞</button>
         </div>
-        <div class="flex flex-col justify-center items-center">
+        <div class="grid grid-cols-7 gap-1 mt-2">
 
-            <div  class="grid grid-cols-7 gap-1 mt-4">
+            <div class="text-gray-300 font-extralight pl-2 text-xs w-6" v-for="d in dayHeadings" :key="d">{{ d }}</div>
+        </div>
+        <div ref="scrollableDiv" @scroll="checkScroll" class="flex flex-col justify-start items-center overflow-scroll ">
 
-                <div class="text-gray-300 font-extralight pl-2 text-xs w-6" v-for="d in dayHeadings" :key="d">{{ d }}</div>
-            </div>
-            <div dir="rtl" class="grid grid-cols-7 gap-1 mt-1">
-                <div v-for="entry in this.entries" :key="entry.id">
-                    <div v-if="entry.id" class="h-6 w-6 border rounded-md"
-                        :class="entry.is_complete ? 'bg-green-200' : 'bg-np-base border-np-base'"></div>
-                    <div v-else class="h-6 w-6 "></div>
+            <div class="">
+
+                <div dir="rtl" class="grid grid-cols-7 gap-1 mt-1 ">
+                    <div v-for="entry in this.entries" :key="entry.id">
+                        <div v-if="entry.id" class="h-6 w-6 border rounded-md"
+                            :class="entry.is_complete ? 'bg-green-200' : 'bg-np-base border-np-base'"></div>
+                        <div v-else class="h-6 w-6 "></div>
+                    </div>
                 </div>
+            </div>
+            <div v-if="showEllipsis" class="flex justify-center bg-np-base">
+                <div class=" absolute -bottom-4 text-2xl text-gray-200 ">. . .</div>
             </div>
         </div>
     </div>
@@ -60,6 +66,10 @@ export default {
             if (this.entries) {
                 this.padEntries()
             }
+            this.$nextTick(() => {
+                this.checkScroll();
+            });
+
         },
         setSelectedLimit(value) {
             this.selected = value
@@ -75,7 +85,19 @@ export default {
             for (let i = 0; i <= missingDays; i++) {
                 this.entries.unshift({ id: null, is_complete: false, timestamp: null, panel_id: null })
             }
-        }
+        },
+        checkScroll() {
+            const element = this.$refs.scrollableDiv
+            if (!element) {
+                console.error("Element not found");
+                return;
+            }
+            if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
+                this.showEllipsis = false
+            } else {
+                this.showEllipsis = true
+            }
+        },
 
     },
     components: {
@@ -89,10 +111,11 @@ export default {
             entries: [],
             sort_key: null,
             sort_direction: null,
-            limit: null,
+            limit: 30,
             offset: null,
-            selected: null,
-            dayHeadings: ['m', 't', 'w', 't', 'f', 's', 's']
+            selected: 30,
+            dayHeadings: ['m', 't', 'w', 't', 'f', 's', 's'],
+            showEllipsis: false
         }
     }
 
