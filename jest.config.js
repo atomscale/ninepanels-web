90ec4ed900d79eeb9e6d07b5ac73c3693b94767f
Ben Dyer <ben@atomscale.co>
2024-02-04 07:29:29 +0000

    finsihed test framework set up

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        customExportConditions: ["node", "node-addons"],
+     },
     moduleFileExtensions: ['js', 'json', 'vue'],
     transform: {
       '^.+\\.vue$': '@vue/vue3-jest',
+      '^.+\\.js$': 'babel-jest',
     },
     moduleNameMapper: {
       '^@/(.*)$': '<rootDir>/src/$1',
